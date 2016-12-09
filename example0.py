87aba225e5a25d2cc1eed39ddc7eec9bddc4c68d
SWolthoff <samuel.wolthoff@vtsd.com>
2016-12-09 08:27:59 -0500

    Test Change

--- example0.py
+++ example0.py
@@ -3,7 +3,7 @@
 #     team_name: a string
 #     strategy_name: a string
 #     strategy_description: a string
-#     move: A function that returns 'c' or 'b'
+#     move: A function that returns 'c' or 'b'gbjkgbkghjk
 ####
 
 team_name = 'E0'
